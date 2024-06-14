e812adb11f30a49946507f50479f8b889367ed22
marcelnoehre <marcelnoehre@web.de>
2024-06-15 00:53:05 +0200

    read file via cmd

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <fstream>
 #include "interpreter.hpp"
 
-int main() {
-    Interpreter interpreter;
+int main(int argc, char *argv[]) {
 
-    std::string code = "print('Hello, world!')";
+    if (argc != 2 ) {
+        std::cerr << "Usage: " << argv[0] << " <filename>.sqz" << std::endl;
+        return 1;
+    }
+    
+    std::string filename = argv[1];
+    std::size_t index = filename.find_last_of(".");
+    if (index == std::string::npos || filename.substr(index + 1) != "sqz") {
+        std::cerr << "Error: File must have a .sqz extension" << std::endl;
+        return 1;
+    }
+
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "Cannot read file: " << filename << std::endl;
+        return 1;
+    }
+
+    std::string code;
+    std::string line;
+    while (std::getline(file, line)) {
+        code += line + "\n";
+    }
+
+    Interpreter interpreter;
     interpreter.interpret(code);
 
     return 0;
