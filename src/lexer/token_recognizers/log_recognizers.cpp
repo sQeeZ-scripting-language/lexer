e837b5d3ef0a274959f6f87962e75cae6c7ce316
Marcel Nöhre <marcel.noehre@entiac.com>
2024-06-17 10:44:33 +0200

    adjust logger checks

--- src/lexer/token_recognizers/log_recognizers.cpp
+++ src/lexer/token_recognizers/log_recognizers.cpp
@@ -1,33 +1,26 @@
 #include "lexer/token_recognizers/log_recognizers.hpp"
 
 Token recognizeColoredLog(Lexer& lexer) {
-    if (checkIfLogKeyword("logc", lexer)) {
-        lexer.currentPosition += 4;
-        return {TokenType::TOKEN_LOG_COLORED, "logc"};
-    }
+    if (checkIfLogKeyword("logc", lexer)) return {TokenType::TOKEN_LOG_COLORED, "logc"};
     return {TokenType::TOKEN_UNKNOWN, ""};
 }
 
 Token recognizeBasicLog(Lexer& lexer) {
-    if (checkIfLogKeyword("log", lexer)) {
-        lexer.currentPosition += 3;
-        return {TokenType::TOKEN_LOG_BASIC, "log"};
-    }
+    if (checkIfLogKeyword("log", lexer)) return {TokenType::TOKEN_LOG_BASIC, "log"};
     return {TokenType::TOKEN_UNKNOWN, ""};
 }
 
 Token recognizeErrorLog(Lexer& lexer) {
-    if (checkIfLogKeyword("error", lexer)) {
-        lexer.currentPosition += 5;
-        return {TokenType::TOKEN_LOG_ERROR, "error"};
-    }
+    if (checkIfLogKeyword("error", lexer)) return {TokenType::TOKEN_LOG_ERROR, "error"};
     return {TokenType::TOKEN_UNKNOWN, ""};
 }
 
 bool checkIfLogKeyword(const std::string& keyword, Lexer& lexer) {
-    return lexer.code.substr(lexer.currentPosition, keyword.size()) == keyword
+    bool isLogger = lexer.code.substr(lexer.currentPosition, keyword.size()) == keyword
         && lexer.currentPosition + keyword.size() < lexer.code.size()
         && !isalpha(lexer.code[lexer.currentPosition + keyword.size()]);
+    if (isLogger) lexer.currentPosition += keyword.size();
+    return isLogger;
 }
 
 void registerLogRecognizers(std::unordered_map<std::string, TokenRecognizer>& tokenRecognizers) {
