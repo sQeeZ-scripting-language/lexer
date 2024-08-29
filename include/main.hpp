73290971a0caf350f8869ea2fd6503bd6e8aa1b7
marcelnoehre <marcelnoehre@web.de>
2024-08-29 08:25:27 +0200

    remove interpreter part

---
+++ include/main.hpp
@@ -0,0 +1,14 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "lexer/lexer.hpp"
+#include "lexer/token.hpp"
+
+void lex(const std::string& code);
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "interpreter.hpp"
+#include "main.hpp"
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -25,8 +25,25 @@ int main(int argc, char *argv[]) {
     code += line + "\n";
   }
 
-  Interpreter interpreter;
-  interpreter.interpret(code);
-
+  lex(code);
   return 0;
+}
+
+std::vector<Token> tokens;
+
+void lex(const std::string& code) {
+  Lexer lexer(code);
+  Token previousToken = {TokenType::TOKEN_UNKNOWN, "Unknown", "Initialize Lexer"};
+
+  do {
+    std::cout << "###Token###" << std::endl;
+    std::cout << "Value: " << previousToken.value << std::endl;
+    std::cout << "Desc: " << previousToken.desc << std::endl;
+    std::cout << "Position: " << lexer.pos << std::endl;
+    std::cout << std::endl;
+
+    tokens.push_back(lexer.getNextToken());
+    
+    previousToken = tokens.back();
+  } while (tokens.back().type != TokenType::TOKEN_EOF);
 }
