81e1d1eb999609b57f326d49154f7f7ec73a5a79
marcelnoehre <marcelnoehre@web.de>
2024-09-27 17:20:13 +0200

    add loops and syntax signs

--- include/lexer/token_recognizers/keyword_recognizers.hpp
+++ include/lexer/token_recognizers/keyword_recognizers.hpp
@@ -15,8 +15,12 @@ void recognizeIf(Lexer& lexer, std::unique_ptr<Token>& tokenPtr);
 void recognizeElse(Lexer& lexer, std::unique_ptr<Token>& tokenPtr);
 void recognizeElseIf(Lexer& lexer, std::unique_ptr<Token>& tokenPtr);
 void recognizeFor(Lexer& lexer, std::unique_ptr<Token>& tokenPtr);
+void recognizeWhile(Lexer& lexer, std::unique_ptr<Token>& tokenPtr);
+void recognizeDo(Lexer& lexer, std::unique_ptr<Token>& tokenPtr);
 void recognizeFunction(Lexer& lexer, std::unique_ptr<Token>& tokenPtr);
 void recognizeReturn(Lexer& lexer, std::unique_ptr<Token>& tokenPtr);
+void recognizeIn(Lexer& lexer, std::unique_ptr<Token>& tokenPtr);
+void recognizeOf(Lexer& lexer, std::unique_ptr<Token>& tokenPtr);
 
 void registerKeywordRecognizers(std::unordered_map<std::string, TokenRecognizer>& tokenRecognizers);
 
